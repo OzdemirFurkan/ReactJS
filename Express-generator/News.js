be3949bc2d68d65d78ad094432aabad0937ac93f
Furkan Ozdemir <furkanozdmrr@hotmail.com>
2018-10-20 04:50:17 -0700

    New projects added

---
+++ Express-generator/News.js
@@ -0,0 +1,14 @@
+import NewsForm from '.NewsForm';
+
+class News extends React.Component{
+    render(){
+        return(
+            <div>
+                {elements}
+                <NewsForm/>
+            </div>
+        )
+    }
+}
+
+export default News;
---
+++ Express-generator/NewsForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NewsForm extends React.Component{
+    render(){
+        return(
+            <div>
+                <button>ADD</button>
+            </div>
+        )
+    }
+}
+
+export default NewsForm;
